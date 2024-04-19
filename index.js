77396932c6aba67abfa5789ea873cbe9720b25be
Kaxh😏 <160304255+Kaxhif01@users.noreply.github.com>
2024-04-19 08:09:04 +0500

    Add files via upload
    first pushup word counter

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+// import inquirer module, which is a command line interface for node.js
+import inquirer from "inquirer";
+// declare a constant 'answers' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+//print th array of words to the console
+console.log(words);
+//print word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
